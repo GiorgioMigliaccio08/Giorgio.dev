8310a4d1a374dd09fa6c1c699282e17866d11df2
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-03-12 17:28:32 +0100

    Add New Componets Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Giorgio Dev</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#link">Project</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another information
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">More</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Last Portfolio
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
