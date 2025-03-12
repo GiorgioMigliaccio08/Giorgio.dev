d7f6dc60c93ea0e503dede11e82bbfb9d281c949
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-03-12 17:28:50 +0100

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
