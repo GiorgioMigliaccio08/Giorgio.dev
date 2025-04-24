4ce85b8bd17e39cec20fed5272c429d26048fb17
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-04-24 19:39:11 +0200

    Modify Navbar 2.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ function BasicExample() {
   return (
     <Navbar expand="lg" style={{ backgroundColor: "#add8e6" }}>
       <Container>
-        <Navbar.Brand href="#home">Giorgio Dev</Navbar.Brand>
+        <Navbar.Brand href="#home">&lt;&gt; Giorgio Dev &lt;/&gt;</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">About</Nav.Link>
-            <Nav.Link href="#projects">Project</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <NavDropdown title="More" id="basic-nav-dropdown">
+            <Nav.Link href="#home">// About</Nav.Link>
+            <Nav.Link href="#projects">// Project</Nav.Link>
+            <Nav.Link href="#contact">// Contact</Nav.Link>
+            <NavDropdown title="// More" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another information
