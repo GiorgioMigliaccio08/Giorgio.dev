f822e5cdcd2fed7fca7f9971daf35e1af86d4662
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-04-24 20:02:42 +0200

    Add Component Home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import Home from "./components/Home";
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Navbar />
+        <Home />
       </div>
     </BrowserRouter>
   );
